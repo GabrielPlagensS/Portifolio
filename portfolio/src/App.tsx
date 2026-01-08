8cd0d5fcf80750f8793b5619be6c9271675c08c4
Gabriel plagens schuinsekel <gabriel.schuinsekel@gmail.com>
2026-01-08 09:17:52 -0300

    Atualização
    Arquivo do projeto em typescript criado

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Ola</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Components/Header.tsx
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+        <div className="bg-[#000000]">
+            <div>
+                <div>
+                    <p>G</p>
+                    <p>Gabriel P. Schuinsekel</p>
+                </div>
+                <div>
+                    <p>sobre</p>
+                    <p>experiencia</p>
+                    <p>Formação e Cursos</p>
+                    <p>Habilidades</p>
+                    <p>Projetos</p>
+                    <p>Contato</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
