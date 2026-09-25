f61fa6d4e2de8a20eac0bfea4301c56a37fd58d2
Gabriel plagens schuinsekel <gabriel.schuinsekel@gmail.com>
2026-09-25 20:30:27 -0300

    Experiencia
    Descrição melhorada

--- portfolio/src/Pages/Experiencia.tsx
+++ portfolio/src/Pages/Experiencia.tsx
@@ -21,7 +21,7 @@ const Experiencia =() => {
             <div className="p-5">
                 <Expecard 
                     title="Estagiário na SMED - Secretaria Municipal de Educação de Ijuí" 
-                    descricao="Atuei no sistema da SMED." 
+                    descricao="Atuei no suporte do sistema da SMED." 
                     data="jul. 2024 - jul. 2026" 
                 />
             </div>
